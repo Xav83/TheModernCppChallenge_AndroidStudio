27245e5348cc98e9496c674aa38854107b6837c5
Xav83 <x.jouvenot@gmail.com>
2020-03-14 12:28:46 +0100

    Adds the cpp function able to compute the solution of the problem

--- TheModernCppChallenge/app/src/main/cpp/native-lib.cpp
+++ TheModernCppChallenge/app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,19 @@
 #include <jni.h>
 #include <string>
 
+[[nodiscard]] constexpr auto sumOf3and5MultipleUpTo(const unsigned int limit)
+{
+    size_t sum = 0;
+    for(auto number = limit; number >= 3; --number)
+    {
+        if(number % 3 == 0 || number % 5 == 0)
+        {
+            sum += number;
+        }
+    }
+    return sum;
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_themoderncppchallenge_MainActivity_stringFromJNI(
         JNIEnv* env,
