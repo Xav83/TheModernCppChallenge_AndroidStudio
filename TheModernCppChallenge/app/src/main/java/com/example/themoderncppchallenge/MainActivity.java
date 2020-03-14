62e18e69efaee656975b6234456442f9434ce6ba
Xav83 <x.jouvenot@gmail.com>
2020-03-14 12:32:34 +0100

    Displays the solution depending on the input given by the user.

--- TheModernCppChallenge/app/src/main/java/com/example/themoderncppchallenge/MainActivity.java
+++ TheModernCppChallenge/app/src/main/java/com/example/themoderncppchallenge/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.themoderncppchallenge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +20,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText et = findViewById(R.id.plain_text_input);
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int limit = 0;
+                if(count != 0)
+                {
+                    limit =  Integer.parseInt(s.toString());
+                }
+                TextView tv = findViewById(R.id.result);
+                tv.setText("Result: " + Sum3And5Multiples(limit));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     /**
