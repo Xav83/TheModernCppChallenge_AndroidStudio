e85fd98282a82acf92360bc9e0b1436d0a504ecc
Xav83 <x.jouvenot@gmail.com>
2020-03-21 18:40:11 +0100

    Adds a button on the activity of the second problem which leads to the activity of the first problem

--- TheModernCppChallenge/app/src/main/java/com/example/themoderncppchallenge/Problem_2.java
+++ TheModernCppChallenge/app/src/main/java/com/example/themoderncppchallenge/Problem_2.java
@@ -2,13 +2,21 @@ package com.example.themoderncppchallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Problem_2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_problem_2);
+    }
+
+    public void goToPreviousProblem(View v)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     public native String Gcd(int i, int j);
