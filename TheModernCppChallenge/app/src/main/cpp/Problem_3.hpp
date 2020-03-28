b6afefa21b8166fdfbcf05c5d195666ff4e31419
Xav83 <x.jouvenot@gmail.com>
2020-03-28 16:47:35 +0100

    Adds the C++ solution for the problem 3

---
+++ TheModernCppChallenge/app/src/main/cpp/Problem_3.hpp
@@ -0,0 +1,7 @@
+#ifndef THEMODERNCPPCHALLENGE_PROBLEM_3_HPP
+#define THEMODERNCPPCHALLENGE_PROBLEM_3_HPP
+
+// This headers include the function std::lcm
+#include <numeric>
+
+#endif //THEMODERNCPPCHALLENGE_PROBLEM_3_HPP
